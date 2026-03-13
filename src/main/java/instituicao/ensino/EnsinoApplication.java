39314d206cf6679a87fd12203f9a1f9133351915
Isabely Martins <isabely.martins@integrada.coop.br>
2026-03-13 20:43:19 -0300

    Subindo o modelo do spring-boot

---
+++ src/main/java/instituicao/ensino/EnsinoApplication.java
@@ -0,0 +1,13 @@
+package instituicao.ensino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsinoApplication.class, args);
+	}
+
+}
